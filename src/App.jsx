1fa5b184f4116d80328dc6116a00a3d376978770
Shashank Naik <shashanknaik808@gmail.com>
2023-11-29 17:26:32 +0530

    Rendered Navbar component with 'textUtils' title and 'About Text Utilities' about prop, along with TextForm component in a container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
-      {/* <Navbar title="Text Util"
-        about="About Text Utilities" /> */}
-        <Navbar />
+      <Navbar title="textUtils"
+        about="About Text Utilities" />
+      <div className='container'>
+        <TextForm />
+      </div>
     </>
   );
 }
