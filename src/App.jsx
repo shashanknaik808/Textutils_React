70b8b0d59bcb6403353b6c0c3e2e81730c3f3fa1
Shashank Naik <shashanknaik808@gmail.com>
2023-11-29 17:35:32 +0530

    Updated TextForm component to use 'props.heading' in the heading and added textarea

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
     <>
       <Navbar title="textUtils"
         about="About Text Utilities" />
-      <div className='container'>
-        <TextForm />
+      <div className='container my-3' >
+        <TextForm heading="Enter the text to analyze" />
       </div>
     </>
   );
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-function TextForm() {
+function TextForm(props) {
     return (
         <>
             <div className='mb-3'>
+                <h1>{props.heading}</h1>
                 <label htmlFor='myBox' className='form-label'>Example textarea</label>
-                <textarea className='form-control' id='myBox' rows="3"></textarea>
-            </div></>
+                <textarea className='form-control' id='myBox' rows="8"></textarea>
+            </div>
+            
+        </>
     )
 }
 
