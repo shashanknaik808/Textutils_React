af9f354aa41db21395a538972822727163043dc5
Shashank Naik <shashanknaik808@gmail.com>
2023-11-29 16:10:42 +0530

    Added PropTypes for 'title' (required) and 'about' in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function Navbar(props) {
     return (
@@ -43,6 +44,10 @@ function Navbar(props) {
     )
 }
 
+Navbar.propTypes = {
+    title: PropTypes.string.isRequired,
+    about: PropTypes.string
+}
 
 
 export default Navbar;
