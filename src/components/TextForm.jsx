e121fb639f9c320c063c1c8fa89ee0f7f97aaa16
Shashank Naik <shashanknaik808@gmail.com>
2023-11-29 18:00:10 +0530

    Added text summary and preview section in TextForm component

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -13,8 +13,9 @@ function TextForm(props) {
         console.log("on change");
         setText(event.target.value)
     }
-        return (
-            <>
+    return (
+        <>
+            <div className='container'>
                 <div className='mb-3'>
                     <h1>{props.heading}</h1>
                     <textarea value={text} onChange={handleOnChange}
@@ -22,8 +23,17 @@ function TextForm(props) {
                     </textarea>
                 </div>
                 <button className='btn btn-primary' onClick={handleUpClick}>Convert to Uppercase</button>
-            </>
-        )
-    }
+            </div>
+            
+            <div className='container my-3'>
+                <h1>Your Text Summary</h1>
+                <p>{text.split(" ").length} words & {text.length} characters</p>
+                <p>{0.7 * text.split(" ").length}seconds to read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
+    )
+}
 
-    export default TextForm;
+export default TextForm;
