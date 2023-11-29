71953b942950cd998045ed2ca0473356ba448607
Shashank Naik <shashanknaik808@gmail.com>
2023-11-29 17:51:33 +0530

    Updated textarea in TextForm component with 'value' and 'onChange' attributes

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import { useState } from 'react';
 
 function TextForm(props) {
-    return (
-        <>
-            <div className='mb-3'>
-                <h1>{props.heading}</h1>
-                <label htmlFor='myBox' className='form-label'>Example textarea</label>
-                <textarea className='form-control' id='myBox' rows="8"></textarea>
-            </div>
-            <button className='btn btn-primary'>Convert to Uppercase</button>
-        </>
-    )
-}
 
-export default TextForm;
+    const [text, setText] = useState('Enter Text Here');
+
+    function handleUpClick() {
+        console.log("On up Click");
+    }
+    function handleOnChange() {
+        console.log("on change");
+    }
+
+    function TextForm(props) {
+        return (
+            <>
+                <div className='mb-3'>
+                    <h1>{props.heading}</h1>
+                    <textarea value={text} onChange={handleOnChange}
+                        className='form-control' id='myBox' rows="8" >
+                    </textarea>
+                </div>
+                <button className='btn btn-primary'>Convert to Uppercase</button>
+            </>
+        )
+    }
+
+    export default TextForm;
