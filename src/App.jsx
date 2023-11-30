9e8b011453641ba5a75f07394c8dacb238b4734f
Shashank Naik <shashanknaik808@gmail.com>
2023-11-30 17:09:21 +0530

    fix: Resolve 'useEffect is not defined' error in src\App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
