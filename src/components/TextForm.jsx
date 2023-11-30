0910823457afbdce4a6c04cbcd06e4ded0bd3895
Shashank Naik <shashanknaik808@gmail.com>
2023-11-30 17:04:17 +0530

    refactor: Removed explicit background and color styles from container

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -37,36 +37,32 @@ function TextForm(props) {
 
     return (
         <>
-            <div className='container' style={{
-                backgroundColor: (props.mode === 'dark') ? '#495057' : '#ffffff',
-                color: (props.mode === 'dark') ? '#ffffff' : '#000000'
-            }}>
-                <div className='container'>
-                    <div className='mb-3'>
-                        <h1>{props.heading}</h1>
-                        <textarea value={text} onChange={handleOnChange}
-                            style={{
-                                backgroundColor: (props.mode === 'dark') ? '#6c757d' : '#ffffff',
-                                color: (props.mode === 'dark') ? '#ffffff' : '#000000'
-                            }}
-                            className='form-control' id='myBox' rows="8" >
-                        </textarea>
-                    </div>
-                    <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
-                    <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lowercase</button>
-                    <button className='btn btn-primary mx-2' onClick={handleClearText}>Clear Text</button>
-                    <button className='btn btn-primary mx-2' onClick={handleCopy}>Copy Text</button>
-                    <button className='btn btn-primary mx-2' onClick={handleExtraSpaces}>Clear Extra Spaces</button>
+            <div className='container'>
+                <div className='mb-3'>
+                    <h1>{props.heading}</h1>
+                    <textarea value={text} onChange={handleOnChange}
+                        style={{
+                            backgroundColor: (props.mode === 'dark') ? '#6c757d' : '#ffffff',
+                            color: (props.mode === 'dark') ? '#ffffff' : '#000000'
+                        }}
+                        className='form-control' id='myBox' rows="8" >
+                    </textarea>
                 </div>
-
-                <div className='container my-3'>
-                    <h1>Your Text Summary</h1>
-                    <p>{text.split(" ").length} words & {text.length} characters</p>
-                    <p>{0.7 * text.split(" ").length}seconds to read</p>
-                    <h2>Preview</h2>
-                    <p>{text}</p>
-                </div>
+                <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
+                <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className='btn btn-primary mx-2' onClick={handleClearText}>Clear Text</button>
+                <button className='btn btn-primary mx-2' onClick={handleCopy}>Copy Text</button>
+                <button className='btn btn-primary mx-2' onClick={handleExtraSpaces}>Clear Extra Spaces</button>
             </div>
+
+            <div className='container my-3'>
+                <h1>Your Text Summary</h1>
+                <p>{text.split(" ").length} words & {text.length} characters</p>
+                <p>{0.7 * text.split(" ").length}seconds to read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        
         </>
     )
 }
