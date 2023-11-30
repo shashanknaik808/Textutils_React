d63d30af9a268587e7dff3cc77f15de9d39aef9c
Shashank Naik <shashanknaik808@gmail.com>
2023-11-30 12:49:26 +0530

    Added dynamic styling for dark mode in TextForm

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -33,26 +33,35 @@ function TextForm(props) {
 
     return (
         <>
-            <div className='container'>
-                <div className='mb-3'>
-                    <h1>{props.heading}</h1>
-                    <textarea value={text} onChange={handleOnChange}
-                        className='form-control' id='myBox' rows="8" >
-                    </textarea>
+            <div className='container' style={{
+                backgroundColor: (props.mode === 'dark') ? '#495057' : '#ffffff',
+                color: (props.mode === 'dark') ? '#ffffff' : '#000000'
+            }}>
+                <div className='container'>
+                    <div className='mb-3'>
+                        <h1>{props.heading}</h1>
+                        <textarea value={text} onChange={handleOnChange}
+                            style={{
+                                backgroundColor: (props.mode === 'dark') ? '#6c757d' : '#ffffff',
+                                color: (props.mode === 'dark') ? '#ffffff' : '#000000'
+                            }}
+                            className='form-control' id='myBox' rows="8" >
+                        </textarea>
+                    </div>
+                    <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
+                    <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lowercase</button>
+                    <button className='btn btn-primary mx-2' onClick={handleClearText}>Clear Text</button>
+                    <button className='btn btn-primary mx-2' onClick={handleCopy}>Copy Text</button>
+                    <button className='btn btn-primary mx-2' onClick={handleExtraSpaces}>Clear Extra Spaces</button>
                 </div>
-                <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to Lowercase</button>
-                <button className='btn btn-primary mx-2' onClick={handleClearText}>Clear Text</button>
-                <button className='btn btn-primary mx-2' onClick={handleCopy}>Copy Text</button>
-                <button className='btn btn-primary mx-2' onClick={handleExtraSpaces}>Clear Extra Spaces</button>
-            </div>
 
-            <div className='container my-3'>
-                <h1>Your Text Summary</h1>
-                <p>{text.split(" ").length} words & {text.length} characters</p>
-                <p>{0.7 * text.split(" ").length}seconds to read</p>
-                <h2>Preview</h2>
-                <p>{text}</p>
+                <div className='container my-3'>
+                    <h1>Your Text Summary</h1>
+                    <p>{text.split(" ").length} words & {text.length} characters</p>
+                    <p>{0.7 * text.split(" ").length}seconds to read</p>
+                    <h2>Preview</h2>
+                    <p>{text}</p>
+                </div>
             </div>
         </>
     )
