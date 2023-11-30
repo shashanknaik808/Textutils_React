d392c07c7da4078d6d651d5c48d11f301a3cb5c4
Shashank Naik <shashanknaik808@gmail.com>
2023-11-30 13:12:21 +0530

    Implemented dynamic alert component with props for messages

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 function Alert(props) {
     return (
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-            <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
+        <>
+            <div className="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>{props.alert.type}</strong>
+                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </>
     )
 }
 
