705bd4e1725c0cbf312161f1f8306d8a48191ba0
Shashank Naik <shashanknaik808@gmail.com>
2023-11-30 11:46:37 +0530

    Added 'mode' state with default value 'light' to Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+
   return (
     <>
       <Navbar title="textUtils"
-        about="About Text Utilities" />
+        about="About Text Utilities" mode={mode} />
       <div className='container my-3' >
         <TextForm heading="Enter the text to analyze" />
       </div>
-      <About />
+      {/* <About /> */}
     </>
   );
 }
