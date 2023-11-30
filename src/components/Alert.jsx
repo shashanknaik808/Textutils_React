751353a9d3b565980f3035202b2be2dc1f93242f
Shashank Naik <shashanknaik808@gmail.com>
2023-11-30 15:26:12 +0530

    Added capitalize function for better alert type formatting

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,9 +1,15 @@
 function Alert(props) {
+
+    function capitalize(word) {
+        const lower = word.toLowerCase();
+        return lower.charAt(0).toUpperCase() + lower.slice(1);
+    }
+
     return (
         <>
             {props.alert && (
                 <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
-                    <strong>{props.alert.type}</strong> : {props.alert.msg}
+                    <strong>{capitalize(props.alert.type)}</strong> : {props.alert.msg}
                     <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                 </div>
             )}
