40e38ee260647224ed35d6812a2b4bfe6bf8fb45
Shashank Naik <shashanknaik808@gmail.com>
2023-11-30 12:17:00 +0530

    Added toggleMode function for switching between light and dark modes

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,13 @@ function App() {
 
   const [mode, setMode] = useState('light');
 
+  function toggleMode() {
+    (mode === 'light') ? (setMode('dark')) : (setMode('light'));
+  }
+
   return (
     <>
-      <Navbar title="textUtils"
-        about="About Text Utilities" mode={mode} />
+      <Navbar title="textUtils" about="About Text Utilities" mode={mode} toggleMode={toggleMode} />
       <div className='container my-3' >
         <TextForm heading="Enter the text to analyze" />
       </div>
