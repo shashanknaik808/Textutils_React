3062e336de939c5b4dcc153c91548f9f8a95b1e5
Shashank Naik <shashanknaik808@gmail.com>
2023-11-30 12:40:01 +0530

    Applied universal dark mode with background color changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,20 @@ function App() {
   const [mode, setMode] = useState('light');
 
   function toggleMode() {
-    (mode === 'light') ? (setMode('dark')) : (setMode('light'));
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#495057'
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = '#ffffff'
+    };
   }
 
   return (
     <>
       <Navbar title="textUtils" about="About Text Utilities" mode={mode} toggleMode={toggleMode} />
       <div className='container my-3' >
-        <TextForm heading="Enter the text to analyze" />
+        <TextForm heading="Enter the text to analyze" mode={mode} />
       </div>
       {/* <About /> */}
     </>
